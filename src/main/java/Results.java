5694e931746bc92cacd7edbf4e1af4b6b4cc3fa2
anachim5 <anachimm@gmail.com>
2019-06-14 02:42:56 +0200

    lol

---
+++ src/main/java/Results.java
@@ -0,0 +1,38 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Iterator;
+/**Klasa definiująca metody które uzyskują z mapy {@link Map} stan złota i wypisują je do pliku .txt*/
+public class Results implements IResults
+{
+    public void beggingOfSimulation() throws IOException
+    {
+        String report="Start of Simulation "+ LocalDateTime.now();
+        BufferedWriter writer = new BufferedWriter(new FileWriter("results.txt", true));
+        writer.write(report);
+        writer.newLine();
+        writer.close();
+    }
+    public void printResults(IMap Mapa,int time) throws IOException
+    {
+        System.out.println("Printing results for tick: "+ time);
+        String report="Result for tick: "+time;
+        BufferedWriter writer = new BufferedWriter(new FileWriter("results.txt", true));
+        writer.write(report);
+        writer.newLine();
+        String Table="Nacja   |   Zloto";
+        writer.write(Table);
+        writer.newLine();
+        Iterator nationIterator=Mapa.getNationsIterator();
+        while(nationIterator.hasNext())
+        {
+            Nation nation= (Nation)nationIterator.next();
+            String strToWrite=nation.getName()+" |  "+String.valueOf(nation.getTresure());
+            writer.write(strToWrite);
+            writer.newLine();
+        }
+        writer.newLine();
+        writer.close();
+    }
+}
